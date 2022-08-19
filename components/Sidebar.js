12628640cf91654f79e182921553b102c5ee1e5f
Juan Guzman <juanltd07@gmail.com>
2022-08-19 14:20:29 -0500

    create feed component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -14,7 +14,7 @@ import {
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-center fixed h-full">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
       {/* Twitter logo */}
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image

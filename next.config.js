6a0c6e485418c8402d7dcb6ae345ae719fac4ef5
Juan Guzman <juanltd07@gmail.com>
2022-08-19 13:36:21 -0500

    create sidebar

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["cdn.cms-twdigitalassets.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

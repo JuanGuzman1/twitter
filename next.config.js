dd79bb96a414e168ec595d338ebbfa3d776b9392
Juan Guzman <juanltd07@gmail.com>
2022-08-22 23:56:35 -0500

    get the session and modify sidebar and input components

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cdn.cms-twdigitalassets.com"],
+    domains: ["cdn.cms-twdigitalassets.com", "lh3.googleusercontent.com"],
   },
 };
 
